2c889baff4a5370bc9f97932e42f0de66d09d8de
justy3 <saurabhjnvj@gmail.com>
2025-09-29 20:22:00 +0800

    fix view functions

--- qt/view.py
+++ qt/view.py
@@ -24,35 +24,38 @@ css_rules = """
 """
 
 formatNumerics = """
-function formatNumber(params){
-	if (params.value !== undefined && params.value !== null){
-		if (params.value === 0){
-			return "0.";
-		}
-		else{
-			if (Math.abs(params.value) < 0.0001) {
-				return params.value.toExponential(2);
-			} else if (Math.abs(params.value) < 0.01) {
-				return params.value.toPrecision(4);
-			} else if (Number.isInteger(params.value)) {
-				return params.value.toLocaleString('en-US', { maximumFractionDigits: 0 });
-			} else {
-				return params.value.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+	function formatNumber(params){
+		if (params.value !== undefined && params.value !== null){
+			if (params.value === 0){
+				return "0.";
+			}
+			else{
+				if (Math.abs(params.value) < 0.0001) {
+					return params.value.toExponential(2);
+				}
+				else if (Math.abs(params.value) < 0.01) {
+					return params.value.toPrecision(4);
+				}
+				else if (Number.isInteger(params.value)) {
+					return params.value.toLocaleString('en-US', { maximumFractionDigits: 0 });
+				}
+				else {
+					return params.value.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+				}
 			}
 		}
+		return "-";
 	}
-	return "-";
-}
-"""
+	"""
 
 url_renderer = """
-function(params) {
-	var link = document.createElement("a");
-	link.href = params.value;
-	link.innerText = params.value;
-	return link;
-}
-"""
+	function(params) {
+		var link = document.createElement("a");
+		link.href = params.value;
+		link.innerText = params.value;
+		return link;
+	}
+	"""
 
 stddev_func = {
 	'std'	: {
@@ -93,21 +96,28 @@ def view(df=None, url_columns=[], show_menu=True, **kw_aggrid):
 		types = dataframe.dtypes
 
 		for field in types.index:
+
 			field_name = str(field)
+
 			if (np.issubdtype(types[field], np.number) or "int" in str(types[field])):
-				value_formatter = formatNumerics if field_name in dataframe and dataframe[field_name].abs().max() > 10_000 else "formatNumber"
+
+				# value_formatter = formatNumerics if field_name in dataframe and dataframe[field_name].abs().max() > 10_000 else "formatNumber"
+				value_formatter = formatNumerics
+
 				column_def = {
-					"cellClass": "number-cell",
-					"headerName": field_name,
-					"field": field_name,
-					"filter": "agNumberColumnFilter",
-					"resizable": True,
-					"sortable": False,
-					"suppressMenu": True,
-					"type": "numericColumn",
+					"cellClass"	:	"number-cell",
+					"headerName":	field_name,
+					"field"		:	field_name,
+					"filter"	:	"agNumberColumnFilter",
+					"resizable"	:	True,
+					"rowGroup"	:	False,
+					"sortable"	:	True,
+					# "suppressMenu":	True,
+					"type": "numberValue",
 					"valueFormatter": value_formatter,
 					"width": 130
 				}
+
 			else:
 				column_def = {
 					"cellClass": "left-align",
@@ -115,10 +125,11 @@ def view(df=None, url_columns=[], show_menu=True, **kw_aggrid):
 					"enablePivot": True,
 					"headerName": field_name,
 					"field": field_name,
-					"filter": "agTextColumnFilter",
+					"filter": "agSetColumnFilter",
 					"resizable": True,
+					"rowGroup" : False,
 					"sortable": False,
-					"width": 220,
+					"width": 200,
 					"pinned": "left" if field == types.index[0] else None
 				}
 
@@ -152,19 +163,20 @@ def view(df=None, url_columns=[], show_menu=True, **kw_aggrid):
 	sidebar = {
 		"hiddenByDefault": False,
 		"toolPanels": [
-			{"id": "columns", "labelDefault": "Columns", "labelKey": "columns", "toolPanel": "agColumnsToolPanel"},
-			{"id": "filters", "labelDefault": "Filters", "labelKey": "filters", "toolPanel": "agFiltersToolPanel"}
+			{"iconKey" : "columns", "id": "columns", "labelDefault": "Columns", "labelKey": "columns", "toolPanel": "agColumnsToolPanel"},
+			{"iconKey" : "filter",	"id": "filters", "labelDefault": "Filters", "labelKey": "filters", "toolPanel": "agFiltersToolPanel"}
 		]
 	}
 
 	grid_options = {
 		"animateRows": True,
 		"columnDefs": column_defs,
-		"enableCharts": True,
 		"enableRangeSelection": True,
+		"enableCharts": True,
 		"groupIncludeFooter": True,
 		"groupIncludeTotalFooter": True,
-		"groupUseEntireRow": False,
+		"rowGroupPanelShow" : "always",
+		# "groupUseEntireRow": False,
 		"sideBar": sidebar,
 		"statusBar": statusBar,
 		"suppressAggFuncInHeader": True,
@@ -180,13 +192,99 @@ def view(df=None, url_columns=[], show_menu=True, **kw_aggrid):
 		height = 600,
 		index=True,
 		menu = {"buttons" : []},
-		paste_from_excel = True,
+		# paste_from_excel = True,
 		quick_filter = True,
-		show_toggle_edit = True,
-		show_toggle_delete = False,
-		sync_on_edit = True,
+		# show_toggle_edit = True,
+		# show_toggle_delete = False,
+		# sync_on_edit = True,
 		theme = "ag-theme-balham-dark",
 	)
 
 	# merge defaults with user args
 	grid_options = {**defaults, **kw_aggrid}
+
+	return Grid(
+		grid_data=dataframe,
+		# license = aggridlicensekey,
+		**grid_options
+	)
+
+# displays a scrollable table vertically and horizontally
+def view2(df, h=30, w=None, **kwargs):
+	CSS_VIEW = """
+	<style>
+		.table {
+			text-align: left;
+			position: relative;
+			border-collapse: collapse;
+			border-spacing: 0;
+			white-space: nowrap;
+		}
+
+		th,
+		td {
+			padding: 0.25rem;
+		}
+
+		tr.red th {
+			background: red;
+			color: white;
+		}
+
+		tr.green th {
+			background: green;
+			color: white;
+		}
+
+		tr.purple th {
+			background: purple;
+			color: white;
+		}
+
+		th {
+			background: black;
+			position: sticky;
+			top: 0;
+			/* Don't forget this, required for the stickiness */
+			box-shadow: 0 2px 2px -1px rgba(0, 0, 0, 0.4);
+		}
+	</style>
+	"""
+
+	hstr = "100%"
+	wstr = "100%"
+
+	if h is not None:
+		h = min(h, len(df)+2)
+		hstr = f"{h*27}px"
+
+	if w is not None:
+		w = f"{w}px"
+
+	# html form of dataframe
+	df_html = df.to_html(classes='table')
+
+	# add scrollability
+	scrollable_df = CSS_VIEW + f"""
+	<html>
+	<head><title>HTML Pandas Dataframe with CSS </title></head>
+	<link rel = "stylesheet" type = "text/css" href = "df_style.css"/>
+	<div style="height:{hstr}; width:{wstr}; overflow:auto;">
+		{df_html}
+	</div>
+	</html>.
+	"""
+	display(HTML(scrollable_df))
+
+
+def view3(df, get_str=False, **kwargs):
+	df_s = df.to_markdown(tablefmt = 'psql', index=False)
+
+	if get_str:
+		return df_s
+
+	else:
+		print(df_s)
+
+def fmt_dict(d: dict):
+	return json.dumps(d, indent=4, sort_keys=True)
